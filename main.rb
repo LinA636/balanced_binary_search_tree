9024df201a44b3770cb796d4a89183a0d0be3564
Lina <117147821+LinA636@users.noreply.github.com>
2023-06-10 09:54:56 +1200

    write driver script

--- main.rb
+++ main.rb
@@ -1,30 +1,48 @@
 require_relative 'tree.rb'
 require_relative 'node.rb'
 
+tree = Tree.new(Array.new(15){rand(1..100)})
+puts "tree balanced?: #{tree.balanced?}"
+unless tree.balanced?
+  tree.rebalance
+  tree.pretty_print
+end
 
-array = (Array.new(15) { rand(1..100) })
-array = [1,4,23,67,43, 75,23,65,34, 65]
-tree = Tree.new(array)
+puts "\nlevel_order:"
+p tree.level_order{|node| node.value}
 
-tree.insert(30)
-tree.insert(77)
-tree.insert(35)
-tree.insert(64)
-tree.insert(66)
-tree.pretty_print()
-puts tree.balanced?
+puts "\ninorder:"
+p tree.inorder{|node| node.value}
 
-tree.delete(67)
-puts tree.balanced?
-tree.delete(75)
-tree.pretty_print()
+puts "\npreorder:"
+p tree.preorder{|node| node.value}
 
-puts tree.balanced?
+puts "\npostorder:"
+p tree.postorder{|node| node.value}
 
-tree2 = Tree.new([2,3, 4, 5])
-puts tree2.balanced?
-tree2.insert(1)
-tree2.pretty_print()
-puts tree2.balanced?
-tree2.rebalance
-tree2.pretty_print()
+val1 = 111
+val2 = 222
+val3 = 333
+puts "\n adding #{val1}, #{val2} and #{val3}:"
+tree.insert(val1)
+tree.insert(val2)
+tree.insert(val3)
+tree.pretty_print
+
+puts "\ntree balanced?: #{tree.balanced?}"
+puts "rebalance:"
+tree.rebalance
+tree.pretty_print
+puts "tree balanced?: #{tree.balanced?}"
+
+puts "\nlevel_order:"
+p tree.level_order{|node| node.value}
+
+puts "\ninorder:"
+p tree.inorder{|node| node.value}
+
+puts "\npreorder:"
+p tree.preorder{|node| node.value}
+
+puts "\npostorder:"
+p tree.postorder{|node| node.value}
